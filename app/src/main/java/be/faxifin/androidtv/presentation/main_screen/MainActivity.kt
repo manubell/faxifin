4441a7e7ced4dcb4c1cd3f0f2e85aad22c65f73a
Manu Bellemans <manu.bellemans@gmail.com>
2021-11-21 23:03:56 +0100

    basic WIP dependency injection + some dependencies

--- app/src/main/java/be/faxifin/androidtv/presentation/main_screen/MainActivity.kt
+++ app/src/main/java/be/faxifin/androidtv/presentation/main_screen/MainActivity.kt
@@ -4,16 +4,12 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import be.faxifin.androidtv.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            val fragment: Fragment = MainFragment()
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.mainActivityFragmentContainer, fragment)
-                .commit()
-        }
     }
 }
